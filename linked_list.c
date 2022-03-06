1d9712d71dc7b3064cc148f2ef2b676b10e391f2
Pedro-vd20 <pedro209.vd@gmail.com>
2022-03-06 17:25:04 +0400

    Partly implemented linked list

--- linked_list.c
+++ linked_list.c
@@ -8,17 +8,35 @@
 linked_list make_list(tcp_packet* packet) {
     linked_list ls;
     
-    ls.head = malloc(sizeof(struct node));
-    ls.tail = ls.head;
-
-    ls.head->p = packet;
-
-    ls.head->next = NULL;
+    ls.head = NULL;
+    add_node(&ls, packet);
 
     return ls;
 }
 
 // add to the end of the list
-// bool add_node(linked_list* ls, tcp_packet packet) {
-    // return true;
-// }
+int add_node(linked_list* ls, tcp_packet* packet) {
+    if(is_empty(ls)) {
+        ls->head = malloc(sizeof(struct node));
+        ls->head->p = packet;
+        ls->tail = ls->head;
+    }
+    else {
+        struct node* newNode = malloc(sizeof(struct node));
+        newNode->p = packet;
+        ls->tail->next = newNode;
+        ls->tail = newNode;
+    }
+    ls->tail->next = NULL;
+}
+
+int is_empty(linked_list* ls) {
+    return ls->head == NULL;
+}
+
+// free all memory 
+int delete_list(linked_list* ls) {
+    while(!is_empty(ls)) {
+        remove_node(ls, 1);
+    }
+}
