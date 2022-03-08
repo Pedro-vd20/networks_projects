fc155b4011cf0cf55cf11e79f0fe7b02bfef0cd4
Pedro-vd20 <pedro209.vd@gmail.com>
2022-03-08 20:24:37 +0400

    Discovered bug. Sliding window doesn't slide

---
+++ test/linked_list.h
@@ -0,0 +1,27 @@
+#ifndef LINKED_LIST
+#define LINKED_LIST
+
+#include "packet.h"
+
+struct node {
+    tcp_packet* p;
+    struct node* next;    
+};
+
+typedef struct {
+    struct node* head;
+    struct node* tail;
+    int size;
+} linked_list;
+
+// add to the end of the list
+int add_node(linked_list*, tcp_packet*);
+tcp_packet* get_head(linked_list*);
+// Remove int items starting from first
+int remove_node(linked_list*, int);
+int is_empty(linked_list*);
+// free all memory 
+int delete_list(linked_list*);
+
+
+#endif
