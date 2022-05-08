03945c41d88686a2d0cc20fbeb6e0d353df4a750
Paulin Alcoser <50881828+pac469@users.noreply.github.com>
2022-05-08 19:10:44 +0400

    before pulling

--- project_2/commands.c
+++ project_2/commands.c
@@ -4,12 +4,13 @@
 
 #include "commands.h"
 
-
-int parse_command(char* msg, char* arg) {
+int parse_command(char *msg, char *arg)
+{
 
     // collect first word of msg
-    char* token = strtok(msg, " ");
-    if(token == NULL) {
+    char *token = strtok(msg, " ");
+    if (token == NULL)
+    {
         return -1;
     }
 
@@ -17,67 +18,84 @@ int parse_command(char* msg, char* arg) {
     strcpy(command, token);
 
     // process commands that don't need argument
-    if(!strcmp(command, "LIST")) {
+    if (!strcmp(command, "LIST"))
+    {
         return 5;
     }
-    else if(!strcmp(command, "!LIST")) {
+    else if (!strcmp(command, "!LIST"))
+    {
         return 6;
     }
-    else if(!strcmp(command, "PWD")) {
+    else if (!strcmp(command, "PWD"))
+    {
         return 9;
     }
-    else if(!strcmp(command, "!PWD")) {
+    else if (!strcmp(command, "!PWD"))
+    {
         return 10;
     }
-    else if(!strcmp(command, "QUIT")) {
+    else if (!strcmp(command, "QUIT"))
+    {
         return 11;
     }
 
     // collect argument
     token = strtok(NULL, " ");
-    if(token == NULL) {
+
+    if (token == NULL)
+    {
         return -2;
     }
 
     strcpy(arg, token);
 
     // process commands with argument
-    if(!strcmp(command, "PORT")) {
+    if (!strcmp(command, "PORT"))
+    {
         return 0;
     }
-    else if(!strcmp(command, "USER")) {
+    else if (!strcmp(command, "USER"))
+    {
         return 1;
     }
-    else if(!strcmp(command, "PASS")) {
+    else if (!strcmp(command, "PASS"))
+    {
         return 2;
     }
-    else if(!strcmp(command, "STOR")) {
+    else if (!strcmp(command, "STOR"))
+    {
         return 3;
     }
-    else if(!strcmp(command, "RETR")) {
+    else if (!strcmp(command, "RETR"))
+    {
         return 4;
     }
-    else if(!strcmp(command, "CWD")) {
+    else if (!strcmp(command, "CWD"))
+    {
         return 7;
     }
-    else if(!strcmp(command, "!CWD")) {
+    else if (!strcmp(command, "!CWD"))
+    {
         return 8;
     }
-        
 
     return -1;
 }
 
-int parse_response(char* msg) {
+int parse_response(char *msg)
+{
     // collect first word
-    char* token = strtok(msg, " ");
-    if(token == NULL) {
+    char *token = strtok(msg, " ");
+    if (token == NULL)
+    {
         return -1;
     }
 
     // check if token is a number
-    for(int i = 0; i < strlen(token); ++i) {
-        if(token[i] < '0' || token[i] > '9') {
+    for (int i = 0; i < strlen(token); ++i)
+    {
+        if (token[i] < '0' || token[i] > '9')
+        {
             return -1;
         }
     }
--- project_2/server.c
+++ project_2/server.c
@@ -23,117 +23,123 @@
 #define ERROR "501 Syntax error in parameters or arguments\n"
 #define LEN_ERROR 44
 #define NOT_IMPLEMENTED "202 Command not implemented\n"
-#define LEN_NOT_IMPLEMENTED 28 
+#define LEN_NOT_IMPLEMENTED 28
 #define AUTHENTICATED "230 User logged in, proceed\n"
-#define LEN_AUTHENTICATED 28 
-
+#define LEN_AUTHENTICATED 28
 
 /**
  * @brief maintains control of connection for each client
- * 
+ *
  * @param arg client information
  */
-void* handle_user(void* arg);
+void *handle_user(void *arg);
 
 /**
  * @brief checks if username matches database
- * 
+ *
  * @param username string to check in db
  * @return int 0 if not authenticated, 1 if found match
  */
-int auth_user(char* username);
+int auth_user(char *username);
 
 /**
  * @brief sees if username and password match
- * 
+ *
  * PREREQ: username has already been matched
- * 
- * @param username 
- * @param password 
+ *
+ * @param username
+ * @param password
  * @return int  0 if no match, 1 if match, -1 if error
  */
-int auth_pass(char* username, char* password);
+int auth_pass(char *username, char *password);
 
-int main() {
+int main()
+{
     // printf("Hello\n");
-    
-    // socket: create the socket 
+
+    // socket: create the socket
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
+    if (sockfd < 0)
+    {
         perror("ERROR opening socket");
         return -1;
     }
 
     // printf("Here\n");
 
-    if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0) {
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0)
+    {
         perror("");
         return -1;
     }
-    
+
     // bind socket
-	struct sockaddr_in server_address;	//structure to save IP address and port
-	memset(&server_address,0,sizeof(server_address)); //Initialize/fill the server_address to 0
-	server_address.sin_family = AF_INET;	//address family
-	server_address.sin_port = htons(CTR_PORT);	//port
-	inet_aton("127.0.0.1", &server_address.sin_addr);
-	if(bind(sockfd,(struct sockaddr *)&server_address,sizeof(server_address))<0) {
-		perror("Bind failed..:");
-		return -1;
-	}
+    struct sockaddr_in server_address;                  // structure to save IP address and port
+    memset(&server_address, 0, sizeof(server_address)); // Initialize/fill the server_address to 0
+    server_address.sin_family = AF_INET;                // address family
+    server_address.sin_port = htons(CTR_PORT);          // port
+    inet_aton("127.0.0.1", &server_address.sin_addr);
+    if (bind(sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
+        perror("Bind failed..:");
+        return -1;
+    }
 
     // printf("Here 1\n");
 
-	// listen on port
-	if(listen(sockfd,5)<0) {
-		perror("Listen Error:");
-		return -1;
-	}
+    // listen on port
+    if (listen(sockfd, 5) < 0)
+    {
+        perror("Listen Error:");
+        return -1;
+    }
 
-	struct sockaddr_in client_address;  //to store the client information
+    struct sockaddr_in client_address; // to store the client information
 
-	// multithread addresses to deal with requests
-	pthread_t thread_ids[NUM_THREADS];
-	int busy[NUM_THREADS]; // keep track of threads currently in use
-	bzero(busy, sizeof(busy)); 
+    // multithread addresses to deal with requests
+    pthread_t thread_ids[NUM_THREADS];
+    int busy[NUM_THREADS]; // keep track of threads currently in use
+    bzero(busy, sizeof(busy));
 
-    while(1) {
+    while (1)
+    {
         // accept new connection from client
-		int client_len = sizeof(client_address);	//lent of client cliend address of type sockaddr_in
-		int client_sd = accept(sockfd,(struct sockaddr*)&client_address,(socklen_t *)&client_len);	//accept the connection but also fill the client address with client info
-		if(client_sd<1)
-		{
-			perror("Accept Error:");
-			return -1;
-		}
+        int client_len = sizeof(client_address);                                                      // lent of client cliend address of type sockaddr_in
+        int client_sd = accept(sockfd, (struct sockaddr *)&client_address, (socklen_t *)&client_len); // accept the connection but also fill the client address with client info
+        if (client_sd < 1)
+        {
+            perror("Accept Error:");
+            return -1;
+        }
 
-		// send thread to handle response
-		client user;
-		user.socket = client_sd;
-		user.address = client_address;
-		int t_id_index = open_thread(busy, NUM_THREADS);
-		// check if index -1 (figure out how to handle later)
-		if(pthread_create(thread_ids + t_id_index, NULL, handle_user, &user) < 0) {
-			perror("multithreading");
-			return -1;
-		}
+        // send thread to handle response
+        client user;
+        user.socket = client_sd;
+        user.address = client_address;
+        int t_id_index = open_thread(busy, NUM_THREADS);
+        // check if index -1 (figure out how to handle later)
+        if (pthread_create(thread_ids + t_id_index, NULL, handle_user, &user) < 0)
+        {
+            perror("multithreading");
+            return -1;
+        }
 
-		// close threads that finish running
-		join_thread(thread_ids, busy, NUM_THREADS);
-	
+        // close threads that finish running
+        join_thread(thread_ids, busy, NUM_THREADS);
     }
 
     close(sockfd);
     return 0;
 }
 
-void* handle_user(void* arg) {
+void *handle_user(void *arg)
+{
     // SERVER IS SUPPOSED TO SEND A 220 FIRST!!!!!!
-    
+
     // collect client info
-    client* user = (client*) arg;
+    client *user = (client *)arg;
     int usersd = user->socket;
-    struct sockaddr_in* user_addr = &(user->address);
+    struct sockaddr_in *user_addr = &(user->address);
 
     // authenticate user
     int auth1 = 0; // flag for username
@@ -142,43 +148,55 @@ void* handle_user(void* arg) {
     bzero(username, sizeof(username));
 
     char buffer[256]; // stores received message
-    char fname[256]; // stores argument in message 
+    char fname[256];  // stores argument in message
     // listen for messages
-    while(1) {
+    while (1)
+    {
         bzero(buffer, sizeof(buffer));
-        bzero(fname, sizeof(fname)); 
+        bzero(fname, sizeof(fname));
 
         // receive command from user
         int bytes = recv(usersd, buffer, sizeof(buffer), 0);
-        if(bytes == 0) {
+        if (bytes == 0)
+        {
             close(usersd);
             printf("Closed!\n");
             break;
         }
 
         int command = parse_command(buffer, fname);
-        if(command == -1) {
+
+        if (command == -1)
+        {
             send(usersd, NOT_IMPLEMENTED, LEN_NOT_IMPLEMENTED, 0);
         }
-        else if(command == -2) {
+        else if (command == -2)
+        {
             send(usersd, ERROR, LEN_ERROR, 0);
         }
-        
+
         // authenticate user
-        if(!(auth1 && auth2)) {
-            
+        if (!(auth1 && auth2))
+        {
+
             // check for receiving username
-            if(!auth1 && command == USER) {
+            if (!auth1 && command == USER)
+            {
+
                 auth1 = auth_user(fname);
-                // error reading 
-                if(auth1 < 0) {
+
+                // error reading
+                if (auth1 < 0)
+                {
                     send(usersd, ERROR, LEN_ERROR, 0);
                 }
-                else if(!auth1) {
+                else if (!auth1)
+                {
                     // return auth error
                     send(usersd, NOT_LOGGED_IN, LEN_NOT_LOGGED_IN, 0);
                 }
-                else {
+                else
+                {
                     // username valid
                     strcpy(username, fname);
                     send(usersd, USERNAME_OK, LEN_USERNAME_OK, 0);
@@ -186,95 +204,108 @@ void* handle_user(void* arg) {
             }
 
             // check for receiving password
-            else if(auth1 && !auth2 && command == PASS) {
+            else if (auth1 && !auth2 && command == PASS)
+            {
                 auth2 = auth_pass(username, fname);
                 // error reading
-                if(auth2 < 0) {
+                if (auth2 < 0)
+                {
                     send(usersd, ERROR, LEN_ERROR, 0);
                 }
                 // not authenticated
-                else if(!auth2) {
+                else if (!auth2)
+                {
                     send(usersd, NOT_LOGGED_IN, LEN_NOT_LOGGED_IN, 0);
                 }
                 // login success
-                else {
+                else
+                {
                     send(usersd, AUTHENTICATED, LEN_AUTHENTICATED, 0);
                 }
             }
             // Not logged in / logging in
-            else {
+            else
+            {
                 send(usersd, NOT_LOGGED_IN, LEN_NOT_LOGGED_IN, 0);
             }
 
             continue;
         }
-    
-
     }
 }
 
-int auth_user(char* username) {
+int auth_user(char *username)
+{
     // open file with usernames
-    FILE* fptr = fopen("users.csv", "r");
-    if(fptr == NULL) {
+    FILE *fptr = fopen("users.csv", "r");
+    if (fptr == NULL)
+    {
         return -1; // error
     }
 
     int auth = 0;
-    
+
     // loop through csv file
     char buffer[256];
     bzero(buffer, sizeof(buffer));
-    while(fgets(buffer, sizeof(buffer), fptr) != 0) {
-        char* user = strtok(buffer, ",");
-        if(user == NULL) {
+    while (fgets(buffer, sizeof(buffer), fptr) != 0)
+    {
+        char *user = strtok(buffer, ",");
+
+        if (user == NULL)
+        {
             return -1;
         }
         // username found
-        else if(strcmp(username, user) == 0) {
+        else if (strcmp(username, user) == 0)
+        {
             auth = 1;
             break;
         }
     }
 
-
     fclose(fptr);
     return auth;
 }
 
-int auth_pass(char* username, char* password) {
+int auth_pass(char *username, char *password)
+{
     // open file with usernames
-    FILE* fptr = fopen("users.csv", "r");
-    if(fptr == NULL) {
+    FILE *fptr = fopen("users.csv", "r");
+    if (fptr == NULL)
+    {
         return -1; // error
     }
 
     int auth = 0;
-    
+
     // loop through csv file
     char buffer[256];
     bzero(buffer, sizeof(buffer));
-    while(fgets(buffer, sizeof(buffer), fptr) != 0) {
+    while (fgets(buffer, sizeof(buffer), fptr) != 0)
+    {
         // remove newline
-        char* tmp = strtok(buffer, "\n");
-        
-        char* token = strtok(tmp, ",");
-        if(token == NULL) {
+        char *tmp = strtok(buffer, "\n");
+
+        char *token = strtok(tmp, ",");
+        if (token == NULL)
+        {
             return -1;
         }
         // username found
-        else if(strcmp(username, token) == 0) {
+        else if (strcmp(username, token) == 0)
+        {
             // get password
             token = strtok(NULL, ",");
-            if(token == NULL) {
+            if (token == NULL)
+            {
                 return -1;
             }
 
             auth = (strcmp(password, token) == 0);
-            break; 
+            break;
         }
     }
-
 
     fclose(fptr);
     return auth;
@@ -282,43 +313,42 @@ int auth_pass(char* username, char* password) {
 
 // int main() {
 
-    /*
-    Bind socket
+/*
+Bind socket
 
-    Start listening port 21
+Start listening port 21
 
-    multithread response
+multithread response
 
-        listen for user commands
+    listen for user commands
 
-        Check user commands (and authenticate)
+    Check user commands (and authenticate)
 
-        Perform action
+    Perform action
 
-        Return message
+    Return message
 
-        IF FILE TRANSFER
+    IF FILE TRANSFER
 
-            Start new thread
+        Start new thread
 
-            Receive user port
+        Receive user port
 
-            Start comm with port 20
+        Start comm with port 20
 
-            Send file    
+        Send file
 
-            Close port
+        Close port
 
-            Close thread
-        
-        IF END COMM
+        Close thread
 
-            close port
+    IF END COMM
 
-            close thread
+        close port
 
-    */
+        close thread
 
+*/
 
-    // return 0;
+// return 0;
 // }
