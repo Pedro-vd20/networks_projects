a11d04351316b40825a4b7423b2e01fd5104ec27
Paulin Alcoser <50881828+pac469@users.noreply.github.com>
2022-05-09 17:08:57 +0400

    added thread parameters

--- project_2/user.h
+++ project_2/user.h
@@ -1,9 +1,20 @@
 #ifndef USER_STRUCT
 #define USER_STRUCT
 
-typedef struct {
+typedef struct
+{
     int socket;
     struct sockaddr_in address;
 } client;
+
+typedef struct
+{
+    int cntr_socket;
+    struct sockaddr_in address;
+    int counter;
+    unsigned short port;
+    char *input;
+
+} thread_parameters;
 
 #endif
